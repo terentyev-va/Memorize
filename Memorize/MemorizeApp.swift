1bc3402a5df309edf7bbfe1a50aa33fd3a0ebb7b
terentyev-va <terentyev.va@yandex.ru>
2022-09-13 13:23:47 +0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Вячеслав Терентьев on 13.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
