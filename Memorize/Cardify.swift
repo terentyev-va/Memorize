e389a32e4b93d79256c296adfea6996c57fddc84
terentyev-va <terentyev.va@yandex.ru>
2022-12-04 08:41:58 +0300

    Добавлен Cardify

---
+++ Memorize/Cardify.swift
@@ -0,0 +1,38 @@
+//
+//  Cardify.swift
+//  Memorize
+//
+//  Created by Вячеслав Терентьев on 04.12.2022.
+//
+
+import SwiftUI
+
+struct Cardify: ViewModifier {
+    
+    var isFaceUp: Bool
+    
+    func body(content: Content) -> some View {
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: DrawingConstants.cornerRadius)
+            if isFaceUp {
+                shape.fill().foregroundColor(.white)
+                shape.stroke(lineWidth: DrawingConstants.lineWidth)
+            } else {
+                shape.fill()
+            }
+            content
+                .opacity(isFaceUp ? 1 : 0)
+        }
+    }
+    
+    private struct DrawingConstants {
+        static let cornerRadius: CGFloat = 10
+        static let lineWidth: CGFloat = 3
+    }
+}
+
+extension View {
+    func cardify(isFaceUp: Bool) -> some View {
+        self.modifier(Cardify(isFaceUp: isFaceUp))
+    }
+}
